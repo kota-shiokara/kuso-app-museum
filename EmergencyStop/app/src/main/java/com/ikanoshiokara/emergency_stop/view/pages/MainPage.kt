4c00d604f20b16cccae92cf9b82034ca73c05a5d
kota-shiokara <kota.book.39@gmail.com>
2022-08-25 23:39:45 +0900

    [ES] メイン機能完成

--- EmergencyStop/app/src/main/java/com/ikanoshiokara/emergency_stop/view/pages/MainPage.kt
+++ EmergencyStop/app/src/main/java/com/ikanoshiokara/emergency_stop/view/pages/MainPage.kt
@@ -1,5 +1,7 @@
 package com.ikanoshiokara.emergency_stop.view.pages
 
+import android.content.Intent
+import android.net.Uri
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -25,10 +27,12 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.core.content.ContextCompat.startActivity
 import com.ikanoshiokara.emergency_stop.LocalNavController
 import com.ikanoshiokara.emergency_stop.NavItem
 import com.ikanoshiokara.emergency_stop.R
@@ -65,6 +69,8 @@ fun MainAppBar() {
 
 @Composable
 fun MainContent() {
+    val context = LocalContext.current
+    val packageName = context.packageName
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
@@ -89,7 +95,12 @@ fun MainContent() {
                         .clip(CircleShape)
                         .background(Color.Red)
                         .clickable {
+                            // val packageInstaller = context.packageManager.packageInstaller
+                            // packageInstaller.uninstall(packageName, intentSender)
 
+                            val packageUri = Uri.parse("package:$packageName")
+                            val uninstallIntent = Intent(Intent.ACTION_UNINSTALL_PACKAGE, packageUri)
+                            startActivity(context, uninstallIntent, null)
                         }
                 )
                 Spacer(modifier = Modifier.size(12.dp))
